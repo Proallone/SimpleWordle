aa3c349442fa0848b2a30857394ed0d941a7d9d3
olowek <proallone14@gmail.com>
2022-10-26 08:05:38 +0200

    activity_main.xml added ids to edittexts, new file deploymentTargetDropDown.xml (dont know where it comes from and what it does but anyway), in MainActivity.kt added a bunch of new code - check if user word is in word list, check if user word is the correct one

--- app/src/main/java/com/example/simplewordle/activities/MainActivity.kt
+++ app/src/main/java/com/example/simplewordle/activities/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.simplewordle.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.example.simplewordle.R
 import com.example.simplewordle.classes.WordleWord
 import com.google.gson.Gson
@@ -18,42 +19,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val wordsJsonFileName= "words.json"
+        val wordsJsonFileName = "words.json"
         val wordsJson = getJsonWordList(wordsJsonFileName)
 
         val wordsListArray = getWordList(wordsJson.toString())
+
         val lastWordIdx = wordsListArray.size
+        val seedThisDay = getTodaySeed()
+        val todayIdx = getRandomWordIndex(seedThisDay, lastWordIdx)
+        val todayWord = wordsListArray[todayIdx]
 
-        val generatedIdx = getRandomWordIndex(lastWordIdx)
-        val generatedWord = wordsListArray[generatedIdx]
-
-        Log.d("dupa", "todays word is $generatedWord")
+        Log.d("today word", todayWord)
 
         val reRollBtn = findViewById<Button>(R.id.re_roll_btn)
-        reRollBtn.setOnClickListener{
-            /*
-            @TODO
-            reroll function
-             */
-            getRandomWordIndex(lastWordIdx)
+        reRollBtn.setOnClickListener {
+            val randomWordIdx= getRandomWordIndex(getRandomSeed(todayIdx), todayIdx)
+            Log.d("random word", wordsListArray[randomWordIdx])
+        }
+
+        val checkBtn = findViewById<Button>(R.id.check_btn)
+        checkBtn.setOnClickListener {
+            var win = false
+            val userInputWord = getUserInputWord()
+            val isInWordList = checkIfInWordList(userInputWord,wordsListArray)
+            Log.d("Isinlist", isInWordList.toString())
+            if(isInWordList) {
+                win = checkWordCorrectness(userInputWord, todayWord)
+            }
+            Log.d("GameOver", win.toString())
         }
 
     }
 
-    private fun getRandomWordIndex(until: Int): Int {
-
+    private fun getTodaySeed(): Int {
         val c = Calendar.getInstance()
-
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
-
-        val seed = year+month+day
-        val from = 0
-
-        return Random(seed).nextInt(from,until)
+        return year + month + day
     }
-    private fun getWordList(wordsJson: String) : List<String>{
+
+    private fun getRandomSeed(until: Int): Int {
+        return Random.nextInt(from=0, until)
+    }
+
+    private fun getRandomWordIndex(seed: Int, until: Int): Int {
+        return Random(seed).nextInt(from = 0, until)
+    }
+
+    private fun getWordList(wordsJson: String): List<String> {
         val gson = Gson()
         val wordsObj = gson.fromJson(wordsJson, WordleWord::class.java)
         return wordsObj.words
@@ -62,12 +76,42 @@ class MainActivity : AppCompatActivity() {
     private fun getJsonWordList(jsonFileName: String): String? {
         var json: String? = null
         try {
-            val  inputStream: InputStream = assets.open(jsonFileName)
-            json = inputStream.bufferedReader().use{it.readText()}
+            val inputStream: InputStream = assets.open(jsonFileName)
+            json = inputStream.bufferedReader().use { it.readText() }
         } catch (ex: Exception) {
             ex.printStackTrace()
             return null
         }
         return json
+    }
+
+    private fun getUserInputWord(): String {
+
+        val et00=findViewById<EditText>(R.id.userInput00)
+        val et01=findViewById<EditText>(R.id.userInput01)
+        val et02=findViewById<EditText>(R.id.userInput02)
+        val et03=findViewById<EditText>(R.id.userInput03)
+        val et04=findViewById<EditText>(R.id.userInput04)
+
+        val char00 = et00.text.toString().lowercase()
+        val char01 = et01.text.toString().lowercase()
+        val char02 = et02.text.toString().lowercase()
+        val char03 = et03.text.toString().lowercase()
+        val char04 = et04.text.toString().lowercase()
+
+        val userInputWord = char00 + char01 + char02 + char03 + char04
+//        val firstLine = findViewById<LinearLayout>(R.id.firstLineLayout)
+//
+//        val u00 = firstLine.getChildAt(0)
+
+        return userInputWord
+    }
+
+    private fun checkWordCorrectness(userWord: String, correctWord: String): Boolean {
+        return (userWord == correctWord)
+    }
+
+    private fun checkIfInWordList(userWord: String, wordList:List<String>):Boolean {
+        return (userWord == wordList.find { word -> word==userWord })
     }
 }
